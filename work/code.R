35d23bb894fb7c4cd97cd55061fd8b82e6a64c9e
Bettina Gruen <bettina.gruen@wu.ac.at>
2026-02-11 18:38:16 +0100

    update code and data

--- work/code.R
+++ work/code.R
@@ -9,7 +9,8 @@ scrapeBacteria <- function(mic = TRUE, dir = tempdir()) {
     url <- "https://mic.eucast.org/Eucast2/SearchController/search.jsp?action=init"
     session1 <- html_session(url)
     f <- html_form(session1)[[1]]
-    antibiotics <- f$fields[[6]]$options # get options for antibiotics
+    antibiotics <- f$fields[["search[antibiotic]"]]$options
+    names(antibiotics) <- trimws(gsub("\\\n", "", names(antibiotics)))
     antibiotics <- antibiotics[antibiotics != "-1"]
     
     ## There is no submit-button in the html, therefore we create one, so submit_form() works
@@ -22,77 +23,96 @@ scrapeBacteria <- function(mic = TRUE, dir = tempdir()) {
     DF <- if (mic) {
               data.frame("Antimicrobial" = character(),
                          "Bacterium" = character(),
-                         "M0.002" = numeric(), "M0.004" = numeric(),
-                         "M0.008" = numeric(), "M0.016" = numeric(),
-                         "M0.032" = numeric(), "M0.064" = numeric(),
-                         "M0.125" = numeric(), "M0.25" = numeric(),
-                         "M0.5" = numeric(), "M1" = numeric(),
-                         "M2" = numeric(), "M4" = numeric(),
-                         "M8" = numeric(), "M16" = numeric(),
-                         "M32" = numeric(), "M64" = numeric(),
-                         "M128" = numeric(), "M256" = numeric(),
-                         "M512" = numeric(), "ECOFF" = numeric(),
+                         "0.002" = numeric(), "0.004" = numeric(),
+                         "0.008" = numeric(), "0.016" = numeric(),
+                         "0.03" = numeric(), "0.06" = numeric(),
+                         "0.125" = numeric(), "0.25" = numeric(),
+                         "0.5" = numeric(), "1" = numeric(),
+                         "2" = numeric(), "4" = numeric(),
+                         "8" = numeric(), "16" = numeric(),
+                         "32" = numeric(), "64" = numeric(),
+                         "128" = numeric(), "256" = numeric(),
+                         "512" = numeric(), 
                          "Distributions" = numeric(),
-                         "Observations" = numeric())
+                         "Observations" = numeric(),
+                         "(T)ECOFF" = character(),
+                         "Confidence interval" = character(),
+                         check.names = FALSE)
           } else {
               data.frame("Antimicrobial" = character(),
                          "Bacterium" = character(),
-                         "DiskContent" = numeric(), "Z6" = numeric(),
-                         "Z7" = numeric(), "Z8" = numeric(),
-                         "Z9" = numeric(), "Z10" = numeric(),
-                         "Z11" = numeric(), "Z12" = numeric(),
-                         "Z13" = numeric(), "Z14" = numeric(),
-                         "Z15" = numeric(), "Z16" = numeric(),
-                         "Z17" = numeric(), "Z18" = numeric(),
-                         "Z19" = numeric(), "Z20" = numeric(),
-                         "Z21" = numeric(), "Z22" = numeric(),
-                         "Z23" = numeric(), "Z24" = numeric(),
-                         "Z25" = numeric(), "Z26" = numeric(),
-                         "Z27" = numeric(), "Z28" = numeric(),
-                         "Z29" = numeric(), "Z30" = numeric(),
-                         "Z31" = numeric(), "Z32" = numeric(),
-                         "Z33" = numeric(), "Z34" = numeric(),
-                         "Z35" = numeric(), "Z36" = numeric(),
-                         "Z37" = numeric(), "Z38" = numeric(),
-                         "Z39" = numeric(), "Z40" = numeric(),
-                         "Z41" = numeric(), "Z42" = numeric(),
-                         "Z43" = numeric(), "Z44" = numeric(),
-                         "Z45" = numeric(), "Z46" = numeric(),
-                         "Z47" = numeric(), "Z48" = numeric(),
-                         "Z49" = numeric(), "Z50" = numeric(),
-                         "ECOFF" = numeric(),
+                         "Disk content" = numeric(), "6" = numeric(),
+                         "7" = numeric(), "8" = numeric(),
+                         "9" = numeric(), "10" = numeric(),
+                         "11" = numeric(), "12" = numeric(),
+                         "13" = numeric(), "14" = numeric(),
+                         "15" = numeric(), "16" = numeric(),
+                         "17" = numeric(), "18" = numeric(),
+                         "19" = numeric(), "20" = numeric(),
+                         "21" = numeric(), "22" = numeric(),
+                         "23" = numeric(), "24" = numeric(),
+                         "25" = numeric(), "26" = numeric(),
+                         "27" = numeric(), "28" = numeric(),
+                         "29" = numeric(), "30" = numeric(),
+                         "31" = numeric(), "32" = numeric(),
+                         "33" = numeric(), "34" = numeric(),
+                         "35" = numeric(), "36" = numeric(),
+                         "37" = numeric(), "38" = numeric(),
+                         "39" = numeric(), "40" = numeric(),
+                         "41" = numeric(), "42" = numeric(),
+                         "43" = numeric(), "44" = numeric(),
+                         "45" = numeric(), "46" = numeric(),
+                         "47" = numeric(), "48" = numeric(),
+                         "49" = numeric(), "50" = numeric(),
                          "Distributions" = numeric(),
-                         "Observations" = numeric())
+                         "Observations" = numeric(),
+                         "(T)ECOFF" = character(),
+                         "Confidence interval" = character(),
+                         check.names = FALSE)
           }  
-  
+
+    nums <- sapply(DF, class) == "numeric"
+    
     ## loop through options 
     for (i in seq_along(antibiotics)) {
         f <- html_form(session1)[[1]]
-        f$fields[[3]]$checked <- NULL
-        f$fields[[4]]$checked <- "checked"
-        f$fields[[5]]$value <- "1000"
-        f$fields[[6]]$value <- antibiotics[i] 
-        f$fields[["submit"]] <- fake_submit
 
-        session1 <- suppressMessages(submit_form(session1, f)) 
-        if (!mic) {
-            session1 <- html_session(str_replace(session1$url, regex("=mic"), "=dif"))
+        f$fields[["search[antibiotic]"]]$value <- antibiotics[i]
+        f$fields[["search[limit]"]]$value <- "1000"
+        
+        ## f$fields[[3]]$checked <- NULL
+        ## f$fields[[4]]$checked <- "checked"
+        ## f$fields[[5]]$value <- "1000"
+        ## f$fields[[6]]$value <- antibiotics[i] 
+        ## f$fields[["submit"]] <- fake_submit
+
+        f$action <- "https://mic.eucast.org/search/"
+
+        if (mic) {
+            f$fields[[2]] <- NULL
+        } else {
+            f$fields[[1]] <- NULL
         }
+        session1 <- suppressMessages(submit_form(session1, f))
         
         ## sometimes no data available = > table doesn't exist = > tryCatch necessary
-        d <- tryCatch(html_table(html_node(read_html(session1), xpath = "/html/body/table[3]"), header = TRUE), 
+        d <- tryCatch(html_table(html_node(read_html(session1), xpath = "/html/body"), header = TRUE), 
                       error = function(e) NA)
-        
-        if (!is.null(dim(d)) && ncol(d) > 1) { 
+        D <- NULL
+        if (!is.null(dim(d)) && ncol(d) > 1) {
             colnames(d)[1] <- "Bacterium"
-            d <- subset(d, Bacterium != "", colnames(d) != "")
-            ## as.numeric() leads to warnings with missing data
-            D <- suppressWarnings(data.frame(Antimicrobial = names(antibiotics)[i], Bacterium = d[, 1],
-                                             as.data.frame(apply(d[, -1, drop = FALSE], 2, function(x) list(as.numeric(x)))),
-                                             stringsAsFactors = FALSE))
-            colnames(D) <- colnames(DF)
-        } else {
-            D <- NULL
+            stopifnot(colnames(DF)[-1] == colnames(d))
+            d <- subset(d, Bacterium != "")
+            if (mic) {
+                d <- subset(d, !`0.002` %in% c("mic.eucast.org", "Provider/service", "No data available"))
+            } else {
+                d <- subset(d, !`Disk content` %in% c("mic.eucast.org", "Provider/service", "No data available"))
+            }
+            if (nrow(d) > 1) {
+                D <- cbind(Antimicrobial = names(antibiotics)[i], d)
+                ## as.numeric() leads to warnings with missing data
+                D[nums] <- suppressWarnings(lapply(D[, nums, drop = FALSE], function(x) as.numeric(x)))
+            }
         }
         ## add data to DF
         DF <- rbind(DF, D)
